6870dbcf42a285333da57ce1d9466cd5bdbed41a
Ruban <80041905+RubanN@users.noreply.github.com>
2021-10-27 00:20:47 +0530

    Simple node and react are connected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button className='btn btn-primary'>Sign Up</button>
+      <form onSubmit={(e) => login(e)}>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input style={{ width: "49%" }} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input style={{ width: "49%" }} type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
   );
 }
-
+function login(e) {
+  e.preventDefault();
+  let resquest = {
+    email: document.getElementById('exampleInputEmail1').value,
+    password: document.getElementById('exampleInputPassword1').value
+  }
+  axios.post('http://localhost:3000/login', resquest)
+    .then(resp => {
+      alert(resp.data.message)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
 export default App;
